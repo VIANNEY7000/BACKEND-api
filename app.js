cd3e6cc4cf1b97285167a20ce1bd9cefd780bfc6
Vianney7000 <vianneyobodozie8@gmail.com>
2025-12-12 03:48:24 -0800

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import  dotenv  from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from "cors"
 import userRouter from './route/user-route.js'
 import producRouter from './route/Product-Route.js'
 
@@ -8,6 +9,8 @@ import producRouter from './route/Product-Route.js'
 dotenv.config()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 
